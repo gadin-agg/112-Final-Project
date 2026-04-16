512203563ea3f47e253d8663c5e1ff7965802bee
GadinAgg <aggarwalgadin@gmail.com>
2026-04-16 17:59:43 -0400

    UI/UX Work, still WIP

--- main.py
+++ main.py
@@ -29,7 +29,13 @@
 # Apply the olive green and beige color scheme to all shapes and labels.
 
 
+# 04/16/26 - Decided to start coding each individual component in the game, then add features as I go. 
+
+
+
+
 import os
+from numpy import size
 import pandas as pd
 from random import sample
 from cmu_graphics import *
@@ -56,6 +62,9 @@ class Asset:
         self.color = color
         self.sharesOwned = 0
 
+    def __repr__(self):
+        return self.ticker
+
     # makes sure the game isnt over / out of bounds
     def getCurrentPrice(self, monthIndex):
         if monthIndex < len(self.priceData):
@@ -64,7 +73,6 @@ class Asset:
 
     def getValue(self, monthIndex):
         return self.sharesOwned * self.getCurrentPrice(monthIndex)
-    
 
 def loadAssets(categories):
     allStocks = []
@@ -99,21 +107,37 @@ def getNetWorth(app):
     return pythonRound(total, 2)
 
 def onAppStart(app):
-    # Basic stuff
-    app.cash = 4000
-    app.monthIndex = 0
-
     categories = {
         'stock_data' : 'forestGreen',
         'index_data' : 'dodgerBlue',
         'crop_data' : 'coral'}
+    
+    # Fake names
+    app.stockNames = {
+    'AAPL':  'Fruit Tech Inc.',
+    'AMZN':  'Jungle Prime Delivery',
+    'CVX':   'Standard Energy Group',
+    'DIS':   'Mouse Kingdom Ent.',
+    'GE':    'General Lightning Co.',
+    'GOOGL': 'Global Search Corp.',
+    'GS':    'Bull & Bear Capital',
+    'INTC':  'Micro-Logic Systems',
+    'JNJ':   'Band-Aid & Wellness',
+    'JPM':   'Heritage Bank & Trust',
+    'KO':    'Bubbly Soda Co.',
+    'MSFT':  'MicroSystem Software',
+    'NFLX':  'Streamline Video',
+    'NKE':   'Swift Footwear Group',
+    'NVDA':  'Nano-Graphics Processing',
+    'SBUX':  'Java Jolt Coffee',
+    'TGT':   'Bullseye Retailers',
+    'V':     'Swipe-O-Matic Credit',
+    'WMT':   'Mega-Mart Supercenters',
+    'XOM':   'Titan Oil & Gas'
+    }
 
     # Loading the stock data, picking random things
-    allS, I, allC = loadAssets(categories)
-
-    app.stocks = sample(allS, 5)
-    app.index = I
-    app.crops = sample(allC, 1)
+    app.allS, app.I, app.allC = loadAssets(categories)
 
     # timing constants
     app.indexRelease = 6
@@ -121,19 +145,120 @@ def onAppStart(app):
     app.cropRelease = 18
     app.goldRelease = 24
 
+    restartApp(app)
+
     print(f"Loaded {len(app.stocks)} stocks, {len(app.index)} index, and {len(app.crops)} crop")
 
-def redrawAll(app):
+def restartApp(app):
+    # Basic stuff
+    app.cash = 4000
+    app.monthIndex = 0
+    app.stepsPerSecond = 1
+    app.gameOver = False
+    app.paused = False
 
+    # released constants
+    app.indexReleased = False
+    app.stockReleased = False
+    app.cropReleased = False
+    app.goldReleased = False
+
+    # picking random sample
+    app.stocks = sample(app.allS, 5)
+    app.index = app.I
+    app.crops = sample(app.allC, 1)
+
+    # reseting ownership
+    for asset in app.allS + app.I + app.allC:
+        asset.sharesOwned = 0
+
+def onStep(app):
+    takeStep(app)
+    
+def takeStep(app):
+    if not app.paused:
+        app.monthIndex += 1
+    if app.monthIndex == app.indexRelease:
+        app.indexReleased = True
+    if app.monthIndex == app.stockRelease:
+        app.stockReleased = True
+    if app.monthIndex == app.cropRelease:
+        app.cropReleased = True
+    if app.monthIndex == app.goldRelease:
+        app.goldReleased = True
+
+def onKeyPress(app, key):
+    if key == 'r':
+        restartApp(app)
+    elif key == 'p':
+        app.paused = not app.paused
+    elif key == 's':
+        takeStep(app)
+
+def drawYearBar(app):
+    width = 160
+    height = 20
+    x = 20
+    y = 50
+    drawRect(x, y, width, height, fill = None, border = 'beige', borderWidth = 1)
+
+    segmentWidth = width / 12
+    month = app.monthIndex % 12
+
+    for i in range(month + 1):
+        drawRect(x + (i * segmentWidth + 2), y + 2, segmentWidth - 4, height - 4, fill='gold')
+
+def drawTile(app, x, y, width, height, assetName, size):
+    color = 'gray' if assetName == 'LOCKED' else 'oldLace'
+    drawRect(x + 7, y + 7, width, height, fill = rgb(75, 75, 75))
+    drawRect(x, y, width, height, fill = color)
+    drawRect(x + 7, y + 7, width - 10, height - 10, fill = None, border = 'black', borderWidth = 3)
+    drawLabel(assetName, x + width/2, y + 30, fill = 'black', size = size, font = 'serif', bold = True, align = 'center')
+
+def drawAssetTiles(app):
+    stockTileWidth = 216
+    stockTileHeight = 240
+    otherTileWidth = 570
+    otherTileHeight = 240
+
+    drawTile(app, 220, 20, otherTileWidth, otherTileHeight, 'Savings Account', 20)
+
+    title = 'Index Fund' if app.indexReleased else 'LOCKED'
+    size = 20 if app.indexReleased else 15
+    drawTile(app, 220 + otherTileWidth + 20, 20, otherTileWidth, otherTileHeight, title, size)
+
+    for i in range(5):
+        title = app.stockNames[app.stocks[i].ticker] if app.stockReleased else 'LOCKED'
+        drawTile(app, 220 + (i * (stockTileWidth + 20)), 280, stockTileWidth, stockTileHeight, title, 15)
+    
+    title = f'{app.crops[0].ticker}' if app.cropReleased else 'LOCKED'
+    size = 20 if app.cropReleased else 15
+    drawTile(app, 220, 540, otherTileWidth, otherTileHeight, f'{app.crops[0].ticker}', 20)
+
+    title = 'Gold' if app.goldReleased else 'LOCKED'
+    size = 20 if app.goldReleased else 15
+    drawTile(app, 220 + otherTileWidth + 20, 540, otherTileWidth, otherTileHeight, title, size)
+
+
+
+def drawSideBar(app):
     # Drawing the side bar
     drawRect(0, 0, 200, app.height, fill= rgb(65, 70, 57))
 
-    drawLabel(f'YEAR {app.monthIndex // 12} OF 10', 20, 30, fill = 'white', size = 20, font = 'serif', bold = True, align = 'left')
-    drawLabel('POCKET CASH', 20, 400, fill = 'white', size = 20, font = 'serif', bold = True, align = 'left')
-    drawLabel(f'${pythonRound(app.cash, 2)}', 20, 430, fill = 'white', size = 25, font = 'serif', bold = True, align = 'left')
-    drawLabel('NET WORTH', 20, 470, fill = 'white', size = 20, font = 'serif', bold = True, align = 'left')
-    drawLabel(f'${getNetWorth(app)}', 20, 500, fill = 'white', size = 25, font = 'serif', bold = True, align = 'left')
+    drawImage('piggy-bank.png', 10, 80, width = 180, height = 180 * 0.54585798816)
+    drawLabel(f'YEAR {app.monthIndex // 12} OF 10', 20, 30, fill = 'beige', size = 20, font = 'serif', bold = True, align = 'left')
+    drawLabel('POCKET CASH', 20, 400, fill = 'beige', size = 12, font = 'serif', bold = True, align = 'left')
+    drawLabel(f'${pythonRound(float(app.cash), 2)}', 20, 420, fill = 'beige', size = 25, font = 'serif', bold = True, align = 'left')
+    drawLabel('NET WORTH', 20, 450, fill = 'beige', size = 12, font = 'serif', bold = True, align = 'left')
+    drawLabel(f'${getNetWorth(app)}', 20, 470, fill = 'beige', size = 25, font = 'serif', bold = True, align = 'left')
+
+    drawYearBar(app)
+
+def redrawAll(app):
+    drawSideBar(app)
+    drawAssetTiles(app)
+
+    # drawLabel(f"Loaded {app.stocks} stocks, {app.index} index, and {app.crops} crop", 20, 550, fill = 'black', size = 20, font = 'serif', bold = True, align = 'left')
 
 
-
-runApp(width=1000, height=800)
+runApp(width=1400, height=800)
